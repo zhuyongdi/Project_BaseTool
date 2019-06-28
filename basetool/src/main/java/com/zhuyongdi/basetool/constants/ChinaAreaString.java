41f23429e44a7615ed0f4f74d25e3f3df1560c15
zhuyongdi <904507761@qq.com>
2019-06-28 16:30:08 +0800

    添加地区ZYDAreaUtil工具类

--- basetool/src/main/java/com/zhuyongdi/basetool/constants/ChinaAreaString.java
+++ basetool/src/main/java/com/zhuyongdi/basetool/constants/ChinaAreaString.java
@@ -106,7 +106,7 @@ public class ChinaAreaString {
     /**
      * 广西
      */
-    public static final String GOANG_XI = "广西";
+    public static final String GUANG_XI = "广西";
 
     /**
      * 海南
@@ -183,6 +183,27 @@ public class ChinaAreaString {
      */
     public static final String GUO_WAI = "国外";
 
-    public static final List<String> AREA_LIST = Arrays.asList(BEI_JING, TIAN_JIN, HE_BEI, SHAN_XI, NEI_MENG_GU, LIAO_NING, JI_LIN, HEI_LONG_JIANG, SHANG_HAI, JIANG_SU, ZHE_JIANG, AN_HUI, FU_JIAN, JIANG_XI, SHAN_DONG, HE_NAN, HU_BEI, HU_NAN, GUANG_DONG, GOANG_XI, HAI_NAN, CHONG_QING, SI_CHUANG, GUI_ZHOU, YUN_NAN, XI_ZANG, SAN_XI, GAN_SU, QING_HAI, NING_XIA, XIN_JIANG, TAI_WAN, XIANG_GANG, AO_MEN, GUO_WAI);
+    /**
+     * 中国所有省份(包括直辖市,自治区,省级行政单位,特别行政区)的集合
+     */
+    public static final List<String> ALL_LIST = Arrays.asList(BEI_JING, TIAN_JIN, HE_BEI, SHAN_XI, NEI_MENG_GU, LIAO_NING, JI_LIN, HEI_LONG_JIANG, SHANG_HAI, JIANG_SU, ZHE_JIANG, AN_HUI, FU_JIAN, JIANG_XI, SHAN_DONG, HE_NAN, HU_BEI, HU_NAN, GUANG_DONG, GUANG_XI, HAI_NAN, CHONG_QING, SI_CHUANG, GUI_ZHOU, YUN_NAN, XI_ZANG, SAN_XI, GAN_SU, QING_HAI, NING_XIA, XIN_JIANG, TAI_WAN, XIANG_GANG, AO_MEN, GUO_WAI);
+
+    /**
+     * 中国直辖市集合
+     * 4个:北京,天津,上海,重庆
+     */
+    public static final List<String> MUNICIPAL_LIST = Arrays.asList(BEI_JING, TIAN_JIN, SHANG_HAI, CHONG_QING);
+
+    /**
+     * 中国自治区集合
+     * 5个:新疆,西藏,宁夏,内蒙古,广西
+     */
+    public static final List<String> AUTONOMOUS_AREA_LIST = Arrays.asList(XIN_JIANG, XI_ZANG, NING_XIA, NEI_MENG_GU, GUANG_XI);
+
+    /**
+     * 中国特别行政区集合
+     * 2个:香港,澳门
+     */
+    public static final List<String> Special_Administrative_REGION_LIST = Arrays.asList(XIANG_GANG, AO_MEN);
 
 }
