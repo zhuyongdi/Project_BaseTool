e5751af15d65a6c5d05b1d6622ac68157178cbd8
zhuyongdi <904507761@qq.com>
2019-07-10 00:22:17 +0800

    更新ScreenTool

--- basetool/src/main/java/com/zhuyongdi/basetool/tool/screen/ScreenTool.java
+++ basetool/src/main/java/com/zhuyongdi/basetool/tool/screen/ScreenTool.java
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -30,16 +32,18 @@ public class ScreenTool {
      * 屏幕信息:屏幕宽度,屏幕高度,密度,像素密度
      */
     public static ScreenInfo getScreenInfo(Context context) {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        WindowManager windowManager = (WindowManager) context.getSystemService(WINDOW_SERVICE);
-        if (windowManager != null) {
-            windowManager.getDefaultDisplay().getMetrics(displayMetrics);
-            ScreenInfo screenInfo = new ScreenInfo();
-            screenInfo.setScreenWidthPx(displayMetrics.widthPixels);
-            screenInfo.setScreenHeightPx(displayMetrics.heightPixels);
-            screenInfo.setDensity(displayMetrics.density);
-            screenInfo.setDensityDpi(displayMetrics.densityDpi);
-            return screenInfo;
+        if (context != null) {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            WindowManager windowManager = (WindowManager) context.getSystemService(WINDOW_SERVICE);
+            if (windowManager != null) {
+                windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+                ScreenInfo screenInfo = new ScreenInfo();
+                screenInfo.setScreenWidthPx(displayMetrics.widthPixels);
+                screenInfo.setScreenHeightPx(displayMetrics.heightPixels);
+                screenInfo.setDensity(displayMetrics.density);
+                screenInfo.setDensityDpi(displayMetrics.densityDpi);
+                return screenInfo;
+            }
         }
         return null;
     }
@@ -48,19 +52,21 @@ public class ScreenTool {
      * 获取屏幕宽度
      */
     public static int getScreenWidth(Context context) {
-        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display;
-        if (windowManager != null) {
-            display = windowManager.getDefaultDisplay();
-            Point outPoint = new Point();
-            if (Build.VERSION.SDK_INT >= 19) {
-                //考虑到了可能有底部虚拟按键的情况
-                display.getRealSize(outPoint);
-            } else {
-                //不可能有底部虚拟按键
-                display.getSize(outPoint);
+        if (context != null) {
+            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            Display display;
+            if (windowManager != null) {
+                display = windowManager.getDefaultDisplay();
+                Point outPoint = new Point();
+                if (Build.VERSION.SDK_INT >= 19) {
+                    //考虑到了可能有底部虚拟按键的情况
+                    display.getRealSize(outPoint);
+                } else {
+                    //不可能有底部虚拟按键
+                    display.getSize(outPoint);
+                }
+                return outPoint.x;
             }
-            return outPoint.x;
         }
         return 0;
     }
@@ -70,19 +76,21 @@ public class ScreenTool {
      */
     @SuppressLint("ObsoleteSdkInt")
     public static int getScreenHeight(Context context) {
-        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display;
-        if (windowManager != null) {
-            display = windowManager.getDefaultDisplay();
-            Point outPoint = new Point();
-            if (Build.VERSION.SDK_INT >= 19) {
-                //考虑到了可能有底部虚拟按键的情况
-                display.getRealSize(outPoint);
-            } else {
-                //不可能有底部虚拟按键
-                display.getSize(outPoint);
+        if (context != null) {
+            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            Display display;
+            if (windowManager != null) {
+                display = windowManager.getDefaultDisplay();
+                Point outPoint = new Point();
+                if (Build.VERSION.SDK_INT >= 19) {
+                    //考虑到了可能有底部虚拟按键的情况
+                    display.getRealSize(outPoint);
+                } else {
+                    //不可能有底部虚拟按键
+                    display.getSize(outPoint);
+                }
+                return outPoint.y;
             }
-            return outPoint.y;
         }
         return 0;
     }
@@ -92,15 +100,17 @@ public class ScreenTool {
      */
     public static int getStatusBarHeight(Context context) {
         int statusBarHeight = -1;
-        try {
-            @SuppressLint("PrivateApi")
-            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
-            Object object = clazz.newInstance();
-            int height = Integer.parseInt(clazz.getField("status_bar_height")
-                    .get(object).toString());
-            statusBarHeight = context.getResources().getDimensionPixelSize(height);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (context != null) {
+            try {
+                @SuppressLint("PrivateApi")
+                Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+                Object object = clazz.newInstance();
+                int height = Integer.parseInt(clazz.getField("status_bar_height")
+                        .get(object).toString());
+                statusBarHeight = context.getResources().getDimensionPixelSize(height);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return statusBarHeight;
     }
@@ -110,8 +120,11 @@ public class ScreenTool {
      */
     @TargetApi(Build.VERSION_CODES.M)
     public static void setStatusBarLightMode(Activity activity) {
-        if (activity.getWindow().getDecorView().getSystemUiVisibility() != (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        if (activity != null) {
+            View decorView = activity.getWindow().getDecorView();
+            if (decorView.getSystemUiVisibility() != (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)) {
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
         }
     }
 
@@ -120,8 +133,11 @@ public class ScreenTool {
      */
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public static void setStatusBarDarkMode(Activity activity) {
-        if (activity.getWindow().getDecorView().getSystemUiVisibility() != (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE)) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if (activity != null) {
+            View decorView = activity.getWindow().getDecorView();
+            if (decorView.getSystemUiVisibility() != (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE)) {
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }
         }
 
     }
@@ -146,8 +162,10 @@ public class ScreenTool {
     public static void setImmersiveStatusBarModeOnlyFullScreen(Activity activity) {
         if (activity != null) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                View decorView = activity.getWindow().getDecorView();
+                Window window = activity.getWindow();
+                View decorView = window.getDecorView();
                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                window.setStatusBarColor(Color.TRANSPARENT);
             }
         }
     }
@@ -169,8 +187,10 @@ public class ScreenTool {
      * 获取沉浸式模式下的状态栏高度,当设备支持沉浸式返回状态栏高度,当设备不支持的时候返回0.
      */
     public static int getImmersiveStatusBarHeight(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return getStatusBarHeight(context);
+        if (context != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                return getStatusBarHeight(context);
+            }
         }
         return 0;
     }
@@ -179,10 +199,13 @@ public class ScreenTool {
      * 获取沉浸式模式下的屏幕高度,当设备支持沉浸式返回屏幕的高度,当设备不支持的时候返回屏幕高度减去状态栏的高度.
      */
     public static int getImmersiveScreenHeight(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return getScreenHeight(context);
+        if (context != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                return getScreenHeight(context);
+            }
+            return getScreenHeight(context) - getStatusBarHeight(context);
         }
-        return getScreenHeight(context) - getStatusBarHeight(context);
+        return 0;
     }
 
 }
