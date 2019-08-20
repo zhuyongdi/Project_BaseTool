589312a05936946c833e720080158dca3330615d
zhuyongdi <904507761@qq.com>
2019-08-20 17:51:30 +0800

    1.添加SwitchButton控件 1.添加系统跳转工具类XXSystemUtil

---
+++ basetool/src/main/java/com/zhuyongdi/basetool/tool/XXSystemUtil.java
@@ -0,0 +1,27 @@
+package com.zhuyongdi.basetool.tool;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+
+public class XXSystemUtil {
+
+    /**
+     * 跳转到权限设置界面
+     */
+    public static void toPermissionSetting(Context context) {
+        Intent mIntent = new Intent();
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= 9) {
+            mIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            mIntent.setData(Uri.fromParts("package", context.getPackageName(), null));
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            mIntent.setAction(Intent.ACTION_VIEW);
+            mIntent.setClassName("com.android.settings", "com.android.setting.InstalledAppDetails");
+            mIntent.putExtra("com.android.settings.ApplicationPkgName", context.getPackageName());
+        }
+        context.startActivity(mIntent);
+    }
+
+}
